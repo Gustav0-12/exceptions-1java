8cc60ad6484c74cd1a2af78a714c9fab2d109c7f
Gustav0-12 <ogustavo13@gmail.com>
2024-04-09 15:58:28 -0300

    Good solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -1,10 +1,10 @@
 package model.entities;
 
 import java.text.SimpleDateFormat;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeFormatterBuilder;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
+
+import model.exceptions.DomainException;
 
 public class Reservation {
 	private Integer roomNumber;
@@ -17,6 +17,9 @@ public class Reservation {
 	}
 
 	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+		if(!checkOut.after(checkIn)){
+			throw new DomainException("Check-out date must be after check-in date");
+		}
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
@@ -43,17 +46,16 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 	
-	public String updateDates(Date checkIn, Date checkOut) {
+	public void updateDates(Date checkIn, Date checkOut) {
 		Date now = new Date();
 		if(checkIn.before(now) || checkOut.before(now)) {
-			return "Reservation dates must be future dates";
+			throw new DomainException("Reservation dates must be future dates");
 		}
 		if(!checkOut.after(checkIn)){
-			return "Check-out date must be after check-in date";
+			throw new DomainException("Check-out date must be after check-in date");
 		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
-		return null;
 	}
 	
 	@Override
@@ -66,7 +68,7 @@ public class Reservation {
 			   + sdf.format(checkOut)
 			   +","
 			   + duration()
-			   + "nights";	   
+			   + " nights";	   
 	}
 	
 	
